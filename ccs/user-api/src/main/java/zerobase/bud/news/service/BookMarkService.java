de64abf082d239a8aa97ecb089eabba65056145d
seos4630@gmail.com <cdoggokk@gmail.com>
2024-05-28 17:14:55 +0900

    fix : delete feign user

--- ccs/user-api/src/main/java/zerobase/bud/news/service/BookMarkService.java
+++ ccs/user-api/src/main/java/zerobase/bud/news/service/BookMarkService.java
@@ -1,31 +1,19 @@
 package zerobase.bud.news.service;
 
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import zerobase.bud.news.client.UserClient;
 import zerobase.bud.news.domain.BookMark;
 import zerobase.bud.news.dto.BookMarkDto;
 import zerobase.bud.news.repository.BookMarkRepository;
 import zerobase.bud.news.repository.BookMarkRepositoryQuerydslImpl;
-import zerobase.bud.news.util.JwtDecoder;
 
 @Service
 public class BookMarkService {
-    private final UserClient userClient;
-
-    @Autowired
-    public BookMarkService(UserClient userClient){
-
-        this.userClient = userClient;
-    }
 
 
     public int getBookMark(Long userId) {
-        int id = userClient.getBookMark(userId);
-        return getCount(id);
+        return getCount(userId);
     }
 
 
@@ -33,7 +21,7 @@ public class BookMarkService {
     private BookMarkRepositoryQuerydslImpl bookMarkRepositoryQuerydslImpl;
 
 
-    public int getCount(int user_id){
+    public int getCount(Long user_id){
        return bookMarkRepositoryQuerydslImpl.fetchCount(user_id);
     }
 
